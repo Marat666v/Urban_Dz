3dbc86d83f28b24c7ce73c31a37e73c370d9cda6
Marat666v <hatapovm@gmail.com>
2024-06-21 21:25:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#first = 68
+#second = 57
+#third = 576
+#if first == second == third:
+#    print(3)
+#elif first == second == third or first == second == third or first == second == third:
+#    print(2)
+#else:
+#    print(0)
