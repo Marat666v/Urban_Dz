a3c4b5119535cf1eca819d2d2ba87369423b53ad
Marat666v <hatapovm@gmail.com>
2024-06-24 17:32:48 +0300

    Add files via upload

--- Name.py
+++ Name.py
@@ -1,5 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 indeks = 0
-while indeks < len(my_list) and my_list[indeks] >= 0:
-    print(my_list[indeks])
-    indeks += 1
+while indeks < len(my_list):
+    if my_list[indeks] > 0:
+        print(my_list[indeks])
+        indeks += 1
+    elif my_list[indeks] == 0:
+        indeks += 1
+    else:
+        break
