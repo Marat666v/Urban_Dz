ee574bd0206f78007896d8f1545ee2bf9f1ad4a0
Marat666v <hatapovm@gmail.com>
2024-06-24 13:08:35 +0300

    Add files via upload

--- Name.py
+++ Name.py
@@ -1,5 +1,5 @@
-print('Hi, Prepodovatel')
-x = 7
-y = 1
-print(x + y)
-print('Kak Nastroi?')
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+indeks = 0
+while indeks < len(my_list) and my_list[indeks] >= 0:
+    print(my_list[indeks])
+    indeks += 1
