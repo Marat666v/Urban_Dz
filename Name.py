ef25f745503859aa22408e7831d040ebc726af92
Marat666v <hatapovm@gmail.com>
2024-07-01 18:45:04 +0300

    Add files via upload

--- Name.py
+++ Name.py
@@ -1,10 +1,5 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-indeks = 0
-while indeks < len(my_list):
-    if my_list[indeks] > 0:
-        print(my_list[indeks])
-        indeks += 1
-    elif my_list[indeks] == 0:
-        indeks += 1
-    else:
-        break
+print('Hi, PuCharm')
+x = 42
+y = 32
+print(x * y)
+print('end Line')
