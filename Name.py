1f60dc0e786830d91e607a3608ab974ab2a79b59
Marat666v <hatapovm@gmail.com>
2024-07-25 15:27:32 +0300

    Add files via upload

--- Name.py
+++ Name.py
@@ -1,5 +1,17 @@
-print('Hi, PuCharm')
-x = 42
-y = 32
-print(x * y)
-print('end Line')
+def send_email(message, recipient, sender = "universiti.help@gmail.com"):
+    if '@' not in recipient or not recipient.endswith(('.ru', '.com' 'net')):
+        print(f'Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>')
+    elif '@' not in sender or not sender.endswith(('.ru', '.com', '.net')):
+        print(f'Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>')
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "universiti.help@gmail.com":
+        print("Письмо успешно отправлено")
+    else:
+        print(f"НЕСТАНЛАРТЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <{sender}> на адрес <{recipient}>")
+
+
+send_email('Это сообщение для проверки в связи','vasyok1337@.gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!','urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправтье задание', 'urban.student@mail.ru', sender ='urban.teacher@mail.uk')
+send_email("Напоминаю самому себе о вебинаре", 'urban.teasher@mail.ru', sender='urban.teasher@mail.ru')
